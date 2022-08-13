399eb8fbe55aee10cf3388a3195a85486828dafd
Assis Júnior <62892419+AssFerj@users.noreply.github.com>
2022-08-13 12:10:43 -0300

    inicio de codigo

---
+++ ex04/index.js
@@ -0,0 +1,13 @@
+let anoAtual = Number(prompt("Digite o ano atual:"))
+let anoNascimento = Number(prompt("Digite o ano do seu nascimento:"))
+let calculo = anoNascimento - 18
+console.log(calculo)
+
+if(){
+
+}else{
+
+}
+
+console.log("Você poderá votar esse ano")
+console.log("Você não poderá votar esse ano")
